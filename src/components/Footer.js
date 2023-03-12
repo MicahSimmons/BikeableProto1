2ac059e6977e724901f6a820d633de0c87752b97
Micah Simmons <sonet_boy@yahoo.com>
2023-03-12 16:34:50 -0700

    Formatting Footer bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,34 @@
+import Image from "next/image"
 import styles from '@/styles/Home.module.css'
+
+function SocialBlock () {
+  return <div className="col-md-12 d-flex align-items-center justify-content-center">
+    <div className={styles.icon}>
+      <a href="/" className="">
+        <Image
+          src="/assets/f_logo_RGB-Blue_58.png"
+          alt="Facebook"
+          width="40"
+          height="40"
+          />
+      </a>
+    </div>
+  </div>
+}
+
+function AddressBlock () {
+  return <div className={"position-absolute end-0 me-2 "+styles.address}>
+    Bikeable Santa Rosa<br />
+    bikeablesr@gmail.com
+  </div>
+}
 
 export default function Footer () {
     return <>
-      <div className={"fixed-bottom "+styles.footer}>
-      <div className="col-md-4 d-flex align-items-center">
-        <a href="/" className="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
-            <svg className="bi" width="30" height="24"></svg>
-        </a>
-        <span className="mb-3 mb-md-0">© 2022 Company, Inc</span>
-      </div>  
-
+      <div className={"fixed-bottom row "+styles.footer}>
+        <SocialBlock />
+        <AddressBlock />
       </div>
+      <div className={styles.footer_spacer}> </div>
     </>
 }  
